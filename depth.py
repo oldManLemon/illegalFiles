a85fd1d1378da4361311c943e0b75fe0d35c3b6f
hase <oldm8reggie@gmail.com>
2018-08-13 16:25:37 +1000

    added flags to trigger emailing

--- depth.py
+++ depth.py
@@ -6,6 +6,7 @@ import log
 rootFolder = settings.scanDir
 find =  settings.illegals
 depth = 0
+flag = 0
 # See https://docs.python.org/3/library/glob.html for Documentation of patterns and glob(Strange name greate module)
 
 # Exists to check if the folder is a job number. If not it will print the name of the folder that is not normal
@@ -23,6 +24,8 @@ def jobDiscovery():
         #print ("Job: ", filename)
         log.logger(f"Job: {filename}")
         scan.scanFolderIgnore(filename)
+        
+
         for suffix in glob.iglob(filename+'/_?/', recursive=True): # Takes Jobnumber and searches for a suffix and returns full path eg J:\2014\14011\_B\
             #print('Sacnning', suffix)
             log.logger(f'Sacnning {suffix}')
--- folderScan.py
+++ folderScan.py
@@ -13,21 +13,26 @@ depth = 0
 #print('Scanning', rootFolder)
 
 
-
+# Scan the Suffix Folders
 def scanFolder(folder):
+    flag = 0
     for dirpath, dirnames, filenames in os.walk(folder):
      for filename in [f for f in filenames if f.endswith(find)]:
+        flag = 1
         #print (os.path.join(dirpath, filename))
         toLog = (os.path.join(dirpath, filename))
         log.logger(toLog)
+        return flag, toLog
 
-
+#Scan the the main folder ignoring suffixes
 def scanFolderIgnore(folder):
-
+    flag = 0
     for dirpath, dirs, filenames in os.walk(folder,topdown=True):
         dirs[:] = [d for d in dirs if d not in settings.exclude]
         for filename in [f for f in filenames if f.endswith(find)]:
+            flag =1
             #print (os.path.join(dirpath, filename))        
             toLog = (os.path.join(dirpath, filename))
             log.logger(toLog)
+            return flag, toLog
 
